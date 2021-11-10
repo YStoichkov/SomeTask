f78262c24414eb93fa94a7808d89838ab09f93d3
Yanko Stoichkov <73684962+YStoichkov@users.noreply.github.com>
2021-11-10 19:06:12 +0200

    Some minor changes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ function solve() {
     const myForm = document.getElementById("myForm");
     const csvFile = document.getElementById("csvFile");
     const timeUnitsButton = document.getElementById('timeUnitsButton')
+    timeUnitsButton.addEventListener('click', mainAppController);
+
     myForm.addEventListener("submit", function (e) {
         e.preventDefault();
+        let h3 = document.createElement('h3');
+        h3.style.color = 'blue';
+        h3.textContent = `File uploaded`
+        document.body.appendChild(h3);
         const input = csvFile.files[0];
         const reader = new FileReader();
         reader.onload = function (e) {
@@ -12,6 +18,9 @@ function solve() {
             localStorage.setItem('data', JSON.stringify(data));
         };
         reader.readAsText(input);
+    });
+
+    function mainAppController() {
         timeUnitsButton.addEventListener('click', (e) => {
             e.preventDefault();
             const data = localStorage.getItem('data')
@@ -21,7 +30,6 @@ function solve() {
             const monthButton = document.getElementById('chooseMonthButton')
             const dayButton = document.getElementById('chooseDayButton')
             const chooseColorButton = document.getElementById('chooseColorButton');
-
 
             let unit = document.getElementById('timeLineUnits');
             var value = unit.options[unit.selectedIndex].value;
@@ -45,24 +53,16 @@ function solve() {
                         let elementEndDateToDate = elementEndDate?.length == 20 ? new Date(elementEndDate) : undefined;
                         let elementStartDateToDate = elementStartDate?.length == 19 ? new Date(elementStartDate) : undefined;
 
-                        if (elementStartDateToDate?.getDay() === 6) {
-                            elementStartDateToDate.setDate(elementStartDateToDate.getDate() + 2);
-                        }
-                        if (elementStartDateToDate?.getDay() === 0) {
-                            elementStartDateToDate.setDate(elementStartDateToDate.getDate() + 1);
-                        }
-                        if (elementEndDateToDate?.getDay() === 6) {
-                            elementEndDateToDate.setDate(elementEndDateToDate.getDate() - 1);
-                        }
-                        if (elementEndDateToDate?.getDay() === 0) {
-                            elementEndDateToDate.setDate(elementEndDateToDate.getDate() - 2);
-                        }
-
                         if (elementEndDateToDate?.getMonth() == month) {
-                            finishedTasks++;
+                            console.log(elementEndDateToDate)
+                            if (elementEndDateToDate?.getDay() !== 6 && elementEndDateToDate?.getDay() !== 0) {
+                                finishedTasks++;
+                            }
                         }
                         if (elementStartDateToDate?.getMonth() == month) {
-                            startedTasks++;
+                            if (elementStartDateToDate?.getDay() !== 6 && elementStartDateToDate?.getDay() !== 0) {
+                                startedTasks++;
+                            }
                         }
                     })
                     createDomElement(startedTasks, finishedTasks, currentMonth)
@@ -137,10 +137,14 @@ function solve() {
                         }
                         for (let index = 0; index <= diffDays; index++) {
                             if (startOfPeriod.setDate(startOfPeriod.getDate() + index) == elementStartDateToDate?.getTime()) {
-                                startedTasks++;
+                                if (elementStartDateToDate?.getDay() !== 6 && elementStartDateToDate?.getDay() !== 0) {
+                                    startedTasks++;
+                                }
                             }
                             if (startOfPeriod.setDate(startOfPeriod.getDate() + index) == elementEndDateToDate?.getTime()) {
-                                finishedTasks++;
+                                if (elementEndDateToDate?.getDay() !== 6 && elementEndDateToDate?.getDay() !== 0) {
+                                    finishedTasks++;
+                                }
                             }
                         }
                     });
@@ -149,27 +153,26 @@ function solve() {
                     startedTasks = 0;
                 })
             }
+            chooseColorButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                let finishedValue = document.getElementById('finishedColor').value;
+                let inProgressColorValue = document.getElementById('inProgressColor').value;
+
+                let finishedBar = document.querySelectorAll('td')[0];
+                finishedBar.style.backgroundColor = finishedValue;
+
+                let inProgressBar = document.querySelectorAll('td')[1];
+                inProgressBar.style.backgroundColor = inProgressColorValue;
+
+
+                let finishedTitle = document.querySelectorAll('th')[0];
+                finishedTitle.style.color = finishedValue;
+
+                let inProgressTitle = document.querySelectorAll('th')[1];
+                inProgressTitle.style.color = inProgressColorValue;
+            })
         })
-        chooseColorButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            let finishedValue = document.getElementById('finishedColor').value;
-            let inProgressColorValue = document.getElementById('inProgressColor').value;
-
-            let finishedBar = document.querySelectorAll('td')[0];
-            finishedBar.style.backgroundColor = finishedValue;
-
-            let inProgressBar = document.querySelectorAll('td')[1];
-            inProgressBar.style.backgroundColor = inProgressColorValue;
-
-
-            let finishedTitle = document.querySelectorAll('th')[0];
-            finishedTitle.style.color = finishedValue;
-
-            let inProgressTitle = document.querySelectorAll('th')[1];
-            inProgressTitle.style.color = inProgressColorValue;
-        })
-    });
-
+    }
 
     function csvToArray(str, delimiter = ",") {
         const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
@@ -187,9 +190,7 @@ function solve() {
 
     function createDomElement(startedTasks, finishedTasks, start, end) {
         document.getElementById('tableBody').innerHTML = '';
-
         let tableBody = document.getElementById('tableBody');
-
         let tr = document.createElement('tr');
         tr.classList.add('qtr');
         tr.setAttribute('id', 1);
@@ -209,23 +210,19 @@ function solve() {
 
         if (startedTasks > finishedTasks) {
             startedTasksTd.style.height = `111px`;
-
             finishedTasksTd.style.height = `90px`;
         }
         if (startedTasks < finishedTasks) {
             startedTasksTd.style.height = `90px`;
-
             finishedTasksTd.style.height = `120px`;
         }
         if (startedTasks == finishedTasks) {
             startedTasksTd.style.height = `111px`;
-
             finishedTasksTd.style.height = `111px`;
         }
         tr.appendChild(th);
         tr.appendChild(startedTasksTd);
         tr.appendChild(finishedTasksTd);
-
         tableBody.appendChild(tr);
     }
 }
